fcbb98f2fb4fd756a035961b771d57d61566c9e2
Annie Ho <girlybit@gmail.com>
2017-12-06 01:56:29 -0800

    updated gnl to free 'saved' variable

---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aho <aho@student.42.us.org>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/06 01:25:10 by aho               #+#    #+#             */
+/*   Updated: 2017/12/06 01:33:23 by aho              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef _GET_NEXT_LINE_H
+# define _GET_NEXT_LINE_H
+
+# include "libft/libft.h"
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# define BUFF_SIZE 20
+
+int get_next_line(const int fd, char **line);
+
+#endif
