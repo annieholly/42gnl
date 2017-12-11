ab203758af32ca601e9b29ea7a28c0afc2c0c63f
Annie Ho <girlybit@gmail.com>
2017-12-11 00:00:02 -0800

    this is a mess

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: aho <aho@student.42.us.org>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/09 00:57:23 by aho               #+#    #+#             */
-/*   Updated: 2017/12/09 23:13:27 by aho              ###   ########.fr       */
+/*   Updated: 2017/12/10 23:51:33 by aho              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,54 +25,36 @@ int ft_strsublen(char *str, int i, char c)
 	return (count); 
 }
 
-int ft_countc(char *str, char c)
+int findnl(char **saved, char **line)
 {
-	int i;
-	int count;
-	i = 0;
-	count = 0;
-	while (str[i] != '\0') 
-	{
-		if (str[i] == c) 
-			count++;
-		i++;
-	}
-	return (count);
-}
-
-int findnl(char **saved, char **line, char *buf)
-{
+//	printf("inside findnl saved:%s--\n", *saved);
 	int i;
 	char *ptrsaved;
-	char *newsaved;
+	
+char *newsaved;
 	char *linestr;
-//	int newsavedlen;
 	i = 0;
-/*
-	if (!(newsaved = ft_strchr(*saved, '\n')))
-	{
-		newsaved = buf;
-	}
-*/
-	newsaved = buf;
-//	printf("inside findnl newsaved:%s--\n", newsaved);
-	ptrsaved = *saved;
-	*saved = ft_strjoin(ptrsaved, buf);
-//	printf("inside findnl strjoin *saved:%s--\n", *saved);
-	ft_strdel(&ptrsaved);
-	if (ft_strchr(*saved, '\n') == NULL) 
+	if (saved[0] == '\0')
 		return (0);
-	ft_strdel(&buf);
-	ptrsaved = *saved;
 
+//	if (ft_strchr(*saved, '\n') == NULL)
+//		return (0);
+	ptrsaved = *saved;
+//	linestr = *saved;
+//	printf("ptrsaved:%s--\n", ptrsaved);
 	if (ptrsaved[0] == '\n')
 	{
+//		printf("ptrsaved[0] newline \n");
 		linestr = ft_strnew(1);
 		linestr[i] = '\0';
 	}
 	if (ft_strchr(ptrsaved, '\n') != NULL && ptrsaved[0] != '\n')
 	{
+//		printf("processing nl\n");
 //		printf("ptrsaved:%s--\n", ptrsaved);
+//		if (linestr != NULL) 
+//			free(linestr);
+
 		linestr = ft_strnew(ft_strsublen(ptrsaved, 0, '\n'));
 		while (ptrsaved[i] != '\n')
 		{
@@ -80,51 +62,120 @@ int findnl(char **saved, char **line, char *buf)
 			i++;
 		}
 		linestr[i] = '\0';
-
 	}
-//	if (newsaved == NULL)
-//		return (0);
-	newsaved = ft_strchr(*saved, '\n') + 1;
-	*saved = strdup(newsaved);
-	
+	if (linestr == NULL) 
+		linestr = *saved;
+
+
 	*line = strdup(linestr);
-//	printf("return - inside findnl saved:%s--\n", *saved);
 //	printf("return - inside findnl line:%s---\n", *line);
 	ft_strdel(&linestr);
-	ft_strdel(&ptrsaved);
+//	ft_strdel(&ptrsaved);
+	if (ft_strchr(ptrsaved, '\n') != NULL)
+	{	
+		if (ptrsaved[0] == '\n' && ft_strlen(ptrsaved) == 1)
+			newsaved = ft_strchr(ptrsaved, '\n');
+		else 
+			newsaved = ft_strchr(ptrsaved, '\n') + 1;
+//		printf("newsaved:%s--\n", newsaved);
+
+
+		*saved = strdup(newsaved);
+
+		ft_strdel(&ptrsaved);
+//		printf("return - inside findnl saved:%s--\n", *saved);
+		return (1);
+	}
+//	printf("return - inside findnl saved:%s--\n", *saved);
+//	ft_strdel(&ptrsaved);
 	return (1);
 }
 
-
 int get_next_line(const int fd, char **line) 
 {
+	printf("\n----GETNEXTLINE CALLED-----\n");
 	int ret;
 	char *buf;
 	static char *saved;
-	if (!(buf = ft_strnew(BUFF_SIZE)))
+	char *ptr_s;
+	if (!(buf = ft_memalloc(BUFF_SIZE + 1)))
 		return (-1);
-//	printf("\n----GETNEXTLINE CALLED-----\n");
-//	printf("saved:%s--\n", saved);
+	printf("saved:%s--\n", saved);
+/*
+	if (saved != NULL && saved[0] == '\0')
+	{
+		ft_strdel(&buf);
+		return (0);
+	}
+*/
 
+//	if (saved != NULL && findnl(&saved, line))
+//	{
+//		return (1);
+//	}
 	if (saved == NULL && !(saved = ft_strnew(0)))
 		return (-1);
-	if (ft_strchr(saved, '\n') != NULL && findnl(&saved, line, buf))
+//	if (ft_strchr(saved, '\n') != NULL && ft_strlen(saved) != 1)	
+
+
+//	if (ft_strchr(saved, '\n') != NULL && findnl(&saved, line) && ft_strlen(saved) != 1)
+//	if (saved != NULL && findnl(&saved, line, buf))
+	if (ft_strchr(saved, '\n') != NULL && ft_strlen(saved) != 1)
 	{
-//		printf("\nreturn 1 - remaining line: %s--\n", *line);	
+		findnl(&saved, line);
+//		printf("bloop\n");
+//		printf("\nreturn 1 - remaining line: %s--\n", *line);
+//		ft_strdel(&buf);
 		return (1);
 	}
 	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
 	{
+//		printf("ret is %d\n", ret);
 		buf[ret] = '\0';
-//		printf("ret: %d\n", ret);
+		ptr_s = saved;
+//		ptr_b = buf;
+//		saved = ft_strjoin(ptr_s, ptr_b);
 //		printf("buf:%s--\n", buf);
-		if (findnl(&saved, line, buf) == 1)
+//		printf("ptr_s:%s--\n", ptr_s);
+		saved = ft_strjoin(ptr_s, buf);
+//		printf("FT_STRJOIN: %i --- saved:%s--\n\n", i, saved);
+		ft_strdel(&ptr_s);
+		if (ft_strchr(saved, '\n') != NULL)
 		{
-			printf("\nreturn 1 - initial read line:%s--\n", *line);
-			return (1);
+//			ft_strdel(&buf);
+			break;
 		}
 	}
-	free(buf);
+
+
+
+
+
+
+
+
+
+//	ft_strdel(&buf);
+	if (findnl(&saved, line) == 1)
+	{
+//		printf("\nreturn 1 - initial read line:%s--\n", *line);
+		ft_strdel(&buf);
+
+
+		if (saved != NULL && saved[0] == '\0')
+		{
+			ft_strdel(&buf);
+			return (0);
+		}
+
+
+
+
+		return (1);
+	}
+	ft_strdel(&buf);
+//	ft_strdel(&saved);
 	close(fd);
+//	printf("return 0\n");
 	return (0);
 }
--- runvalgrind.sh
+++ runvalgrind.sh
@@ -1,2 +1,2 @@
 gcc -Wall -Wextra -Werror -o run *.c ./libft/*.c -g
-valgrind --leak-check=full ./run testfiles/fullmeatstory.txt
+valgrind --leak-check=full ./run testfiles/abc.txt
